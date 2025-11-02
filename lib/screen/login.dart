c6d68aab140aad7e99f97b44edfb12e10cea73f9
I-JasonLee <joshuajasonlee@gmail.com>
2025-11-03 02:04:59 +0700

    Create page login scaffold awal

---
+++ lib/screen/login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: const Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
